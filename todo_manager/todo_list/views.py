c4baa994a1ab9bd213af79a4adf5babb1cedd269
Suren Khorenyan <surenkhorenyan@gmail.com>
2024-04-20 10:24:11 +0300

    ToDo List / Detail views, inner urls

--- todo_manager/todo_list/views.py
+++ todo_manager/todo_list/views.py
@@ -1,3 +1,37 @@
+from django.http import (
+    HttpRequest,
+    HttpResponse,
+)
 from django.shortcuts import render
+from django.views.generic import (
+    ListView,
+    DetailView,
+)
 
-# Create your views here.
+from .models import ToDoItem
+
+
+def index_view(request: HttpRequest) -> HttpResponse:
+    todo_items = ToDoItem.objects.all()[:3]
+    return render(
+        request,
+        template_name="todo_list/index.html",
+        context={"todo_items": todo_items},
+    )
+
+
+class ToDoListIndexView(ListView):
+    template_name = "todo_list/index.html"
+    queryset = ToDoItem.objects.all()[:3]
+
+
+class ToDoListView(ListView):
+    model = ToDoItem
+
+
+class ToDoListDoneView(ListView):
+    queryset = ToDoItem.objects.filter(done=True).all()
+
+
+class ToDoDetailView(DetailView):
+    model = ToDoItem
