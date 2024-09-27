239f868e22d032b8a4084443a8972f54f5159dcf
Suren Khorenyan <surenkhorenyan@gmail.com>
2024-09-27 08:49:14 +0300

    Create email_newsletters app

--- todo_manager/todo_manager/settings.py
+++ todo_manager/todo_manager/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    #
     "todo_list.apps.TodoListConfig",
+    "email_newsletters.apps.EmailNewslettersConfig",
 ]
 
 MIDDLEWARE = [
